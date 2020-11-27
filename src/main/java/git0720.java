660f26f08d2eb644bafc57d8b771d07633ec7f09
LGL <1145131484@qq.com>
2020-11-27 16:59:33 +0800

    hot-fix commit

--- src/main/java/git0720.java
+++ src/main/java/git0720.java
@@ -2,5 +2,6 @@ public class git0720 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("变色");
+        System.out.println("hot-fix");
     }
 }
